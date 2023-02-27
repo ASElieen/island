2cd4df6558c8fd008c0d3399b1c5e5bae51e99c9
username <790768275@qq.com>
2023-02-27 15:38:00 +0800

    feat:接入热更新

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -1,9 +1,11 @@
 import { createServer as createViteDevServer, ViteDevServer } from 'vite'
 import { pluginIndexHtml } from '../plugin-island/indexHTML'
+import pluginReact from '@vitejs/plugin-react'
+
 
 export const createDevServer = async (root: string = process.cwd()): Promise<ViteDevServer> => {
     return createViteDevServer({
         root,
-        plugins: [pluginIndexHtml()]
+        plugins: [pluginIndexHtml(), pluginReact()]
     })
 }
--- src/plugin-island/indexHTML.ts
+++ src/plugin-island/indexHTML.ts
@@ -2,11 +2,28 @@ import { readFile } from 'fs/promises'
 import { Plugin } from 'vite'
 import { DEFAULT_HTML_PATH, CLIENT_ENTRY_PATH } from '../node/constants'
 
-export const pluginIndexHtml = (): Plugin => {
+
+export function pluginIndexHtml(): Plugin {
     return {
         name: 'island:index.html',
         //指名在build还是serve模式调用
         apply: 'serve',
+        //插入入口script标签
+        transformIndexHtml(html) {
+            return {
+                html,
+                tags: [
+                    {
+                        tag: "script",
+                        attrs: {
+                            type: "module",
+                            src: `/@fs/${CLIENT_ENTRY_PATH}`,
+                        },
+                        injectTo: "body",
+                    },
+                ],
+            };
+        },
         //配置devServer的钩子 添加自定义中间件
         configureServer(server) {
             //返回函数注入在内部中间件运行后运行的后置中间件
@@ -14,6 +31,13 @@ export const pluginIndexHtml = (): Plugin => {
                 server.middlewares.use(async (req, res, next) => {
                     let html = await readFile(DEFAULT_HTML_PATH, 'utf-8')
                     try {
+                        //转换index.html专用钩子
+                        html = await server.transformIndexHtml(
+                            req.url,
+                            html,
+                            req.originalUrl
+                        )
+
                         res.statusCode = 200
                         res.setHeader('Content-Type', 'text/html')
                         res.end(html)
